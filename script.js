b6077a3ed345e62428c1a973bf178dd4d1d7f87f
MarcalekM <120452154+MarcalekM@users.noreply.github.com>
2023-03-22 18:49:41 +0100

    Folytatva

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+let osszeg = 0;
+document.getElementById("ossz").innerHTML = osszeg + " Ft";
+
 function AddNewRow(name, size, price) {
     let table = document.getElementById("MyOrder");
-    let x = name;
-    let y = size;
-    let z = price;
     let row = table.insertRow(-1);
     let cell1 = row.insertCell(0);
     let cell2 = row.insertCell(1);
     let cell3 = row.insertCell(2);
-    cell1.innerHTML = x;
-    cell2.innerHTML = y;
-    cell3.innerHTML = z;
+    cell1.innerHTML = name;
+    cell2.innerHTML = size + " cm";
+    cell3.innerHTML = price + " Ft";
   }
 
 function Osszeg(price){
-    let ar = price;
-    let now = document.getElementById("ossz").value;
-    document.getElementById("ossz").value = ar + now;
+    osszeg += price;
+    document.getElementById("ossz").innerHTML = osszeg + " Ft";
+}
+
+function prod1() {
+  let name = "Magyaros";
+  let size = 32;
+  let price = 3000;
+  AddNewRow(name, size, price);
+  Osszeg(price);
+}
+
+function prod2() {
+  let name;
+  let size;
+  let price;
+  AddNewRow(name, size, price);
+  Osszeg(price);
 }
